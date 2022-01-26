4392afe1c6934bb840d21da9f3cd34126f96af15
Dave Stothers <stotherd@gmail.com>
2022-01-26 22:13:26 +0000

    Functional test

--- test/pageObjects/simpleSearchPageObject.js
+++ test/pageObjects/simpleSearchPageObject.js
@@ -9,6 +9,26 @@ class simpleSearchPageObject extends ParentPageObject {
   verifyHomePage () {
     this.isElementEqualToExpected($('h2=Recommended For You'), 'Recommended For You')
   }
+
+  acceptCookies() {
+    $('[id=onetrust-accept-btn-handler]').click()
+  }
+  selectSearchButton() {
+    $('[name="query"]').click()
+  }
+
+  setSearchText(string) {
+    const element = $('//*[@id="home_root"]/div[1]/nav/div[1]/div[1]/div[2]/div/div/div/div[1]/div[1]/input')
+    element.click()
+    element.setValue(string)
+    const errorMessage = 'actual does not equal expected'
+    expect(element.getValue(), errorMessage).to.equal(string)
+  }
+
+  checkResults(string) {
+    const element = $('/html/body/div[2]/div[1]/nav/div[1]/div[1]/div[2]/div/div/div/div[2]/div/div[1]/div/div[2]/div[1]/div/a[1]/div/span[1]')
+    this.isElementEqualToExpected(element, string)
+  }
 }
 
 module.exports = simpleSearchPageObject
--- test/stepDefs/simpleSearchStepDef.js
+++ test/stepDefs/simpleSearchStepDef.js
@@ -1,4 +1,4 @@
-const { Given } = require('cucumber')
+const { Given, When, Then, By} = require('cucumber')
 const SimpleSearchPageObject = require('../pageObjects/simpleSearchPageObject')
 
 const simpleSearchPageObject = new SimpleSearchPageObject()
@@ -6,4 +6,19 @@ const simpleSearchPageObject = new SimpleSearchPageObject()
 Given('I am on the studentbeans homepage', function () {
   simpleSearchPageObject.goToHomePage()
   simpleSearchPageObject.verifyHomePage()
+
+})
+
+Given('I open the search bar', function () {
+  simpleSearchPageObject.acceptCookies()
+  simpleSearchPageObject.selectSearchButton()
+})
+
+When('I enter {string}', function (search_term) {
+  simpleSearchPageObject.setSearchText(search_term)
+
+})
+
+Then('I should be shown a search listing for {string}', function (search_term) {
+  simpleSearchPageObject.checkResults(search_term)
 })
